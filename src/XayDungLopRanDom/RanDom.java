f3d6a98b60a9fd2731c75db832e658499a7bb0df
1234567890 <phantiensy195@gmail.com>
2018-06-27 15:48:06 +0700

    XayDungLopRanDom1

--- src/XayDungLopRanDom/RanDom.java
+++ src/XayDungLopRanDom/RanDom.java
@@ -5,11 +5,11 @@ import java.util.Random;
 public class RanDom {
     private int number;
 
-    public RanDom(int number) {
+    public RanDom(int numb) {
+        this.number = numb;
     }
 
     public int random(int number) {
-        this.number = number;
         Random rd = new Random();
         int num = rd.nextInt(number);
         return num;
